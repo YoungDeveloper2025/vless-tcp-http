894945c56a2536b9c80f5b6c5502607a7ef4117b
onedev <49806461+YoungDeveloper2025@users.noreply.github.com>
2026-08-30 00:26:50 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ email=$(jq -r '.email' config.json)
 port=$(jq -r '.port' config.json)
 
 # Fake HTTP host
-fakeHost="hostdl.com"
+fakeHost="amp-api-edge.apps.apple.com"
 
 # Install Xray
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --version v1.8.23
